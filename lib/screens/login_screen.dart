36d0c9b1cff50c3f7bbd0eb59dddc3432cea201b
Aldebaran Galaviz <aldebarangalaviz@gmail.com>
2026-02-10 13:33:08 -0600

    feat(login): add email and password inputs

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -9,16 +9,65 @@ class LoginScreen extends StatefulWidget {
 }
 
 class _LoginScreenState extends State<LoginScreen> {
+  
+  // Control para mostrar/ocultar la contraseña
+  bool _obscureText = true;
+  
   @override
   Widget build(BuildContext context) {
+    // Para tener el tamaño de la pantalla del dispositivo
+    final Size size = MediaQuery.of(context).size;
     return Scaffold(
       // Evita que se quite espacio del nudge
       body: SafeArea(
-        child: Column(
-          children: [
-            Expanded(child: 
-            RiveAnimation.asset('assets/animated_login_bear.riv'))
-          ]
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 20.0),
+          child: Column(
+            children: [
+              SizedBox(
+                width: size.width,
+                height: 200,
+                child: RiveAnimation.asset('assets/animated_login_bear.riv')
+              ),
+              // widget para separar
+              const SizedBox(height: 10),
+              TextField(
+                // Muestra un tipo de teclado
+                keyboardType: TextInputType.emailAddress,
+                decoration: InputDecoration(
+                  hintText: 'Email',
+                  prefixIcon: const Icon(Icons.email),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(12),
+                  )
+                ),
+              ),
+              const SizedBox(height: 10),
+              TextField(
+                obscureText: _obscureText,
+                decoration: InputDecoration(
+                  hintText: 'Password',
+                  prefixIcon: const Icon(Icons.lock),
+                  suffixIcon: IconButton(
+                    icon: Icon(
+                      // if ternario
+                      _obscureText ? Icons.visibility : Icons.visibility_off,
+                    ),
+                    onPressed: () {
+                      // Refresca el Ícono
+                      setState(() {
+                        _obscureText = !_obscureText;
+                      });
+                    },
+                  ),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(12),
+                  ),
+                ),
+              ),
+              SizedBox(height: 10)
+            ]
+          ),
         )
       )
     );
