5cf2cdfb135915864db97df354e6be813d2949f8
Aldebaran Galaviz <aldebarangalaviz@gmail.com>
2026-02-12 10:48:13 -0600

    feat(rive): wire up isChecking/isHandsUp inputs on login

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -12,6 +12,14 @@ class _LoginScreenState extends State<LoginScreen> {
   
   // Control para mostrar/ocultar la contraseña
   bool _obscureText = true;
+
+  // Crear el cerebro de la animación
+  StateMachineController? _controller;
+  //SMI: State Machine Input
+  SMIBool? _isChecking;
+  SMIBool? _isHandsUp;
+  SMITrigger? _trigSuccess;
+  SMITrigger? _trigFail;
   
   @override
   Widget build(BuildContext context) {
@@ -27,11 +35,42 @@ class _LoginScreenState extends State<LoginScreen> {
               SizedBox(
                 width: size.width,
                 height: 200,
-                child: RiveAnimation.asset('assets/animated_login_bear.riv')
+                child: RiveAnimation.asset('assets/animated_login_bear.riv',
+                stateMachines: ['Login Machine'],
+                // Al iniciar la animación
+                onInit: (artboard) {
+                  _controller = StateMachineController.fromArtboard(
+                    artboard, 
+                    'Login Machine',
+                  );
+
+                  // Verifica que inició bien
+                  if (_controller == null) return;
+
+                  // Agregar el controlador al tablero/escenario
+                  artboard.addController(_controller!);
+                  _isChecking = _controller!.findSMI('isChecking');
+                  _isHandsUp = _controller!.findSMI('isHandsUp');
+                  _trigSuccess = _controller!.findSMI('trigSuccess');
+                  _trigFail = _controller!.findSMI('trigFail');
+
+                },
+                ),
               ),
               // widget para separar
               const SizedBox(height: 10),
+              // Campo de texto Email
               TextField(
+                onChanged: (value) {
+                  if (_isHandsUp != null){
+                    // No tapes los ojos al ver el email
+                    _isHandsUp!.change(false);
+                  }
+                  //Si isChecking es nulo
+                  if (_isChecking == null) return;
+                  // activar el modo chismoso
+                  _isChecking!.change(true);
+                },
                 // Muestra un tipo de teclado
                 keyboardType: TextInputType.emailAddress,
                 decoration: InputDecoration(
@@ -43,7 +82,18 @@ class _LoginScreenState extends State<LoginScreen> {
                 ),
               ),
               const SizedBox(height: 10),
+              // Campo de texto contraseña
               TextField(
+                onChanged: (value) {
+                  if (_isChecking != null){
+                    // No quiero modo chismoso
+                    _isChecking!.change(false);
+                  }
+                  //Si isHandsUp es nulo
+                  if (_isHandsUp == null) return;
+                  // activar el modo chismoso
+                  _isHandsUp!.change(true);
+                },
                 obscureText: _obscureText,
                 decoration: InputDecoration(
                   hintText: 'Password',
